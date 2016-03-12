50a342438af9342d9449acb67c0b6a0e62655da6
cfulloa <cfulloa@gmail.com>
2016-03-11 19:07:07 -0500

    Controlador

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
-/*
-var app = angular.module('demo',[]);
-
-app.controller('StudentsController', function($scope){
-
-    $scope.students = [
-        {name: 'uno', lastName: 'Apl1'},
-        {name: 'dos', lastName: 'Apl2'},
-        {name: 'tres', lastName: 'Apl3'},
-        {name: 'Cuatro', lastName: 'Otro APll'}
-    ];
-});
-
-*/
-
-function(){
+function {} {
     'use strict';
+
     angular.module('demo',[]).controller('StudentsController', StudentsController);
 
     /* para el minificador */
-    StudentsController.$inject = ['$scope'];
+    StudentsController.$inject = ['$scope','$rootScope'];
     /**/
 
 
-    function StudentsController($scope){
+    function StudentsController($scope, $rootScope){
         $scope.students = [
             {name: 'uno', lastName: 'Apl1'},
             {name: 'dos', lastName: 'Apl2'},
             {name: 'tres', lastName: 'Apl3'},
             {name: 'Cuatro', lastName: 'Otro APll'}
             ];
+        console.log('$scope');
+        console.log($scope);
 
-    }
+    };
+
 
 
 }();
+
+/*
+ var app = angular.module('demo',[]);
+
+ app.controller('StudentsController', function($scope){
+
+ $scope.students = [
+ {name: 'uno', lastName: 'Apl1'},
+ {name: 'dos', lastName: 'Apl2'},
+ {name: 'tres', lastName: 'Apl3'},
+ {name: 'Cuatro', lastName: 'Otro APll'}
+ ];
+ });
+
+ */
